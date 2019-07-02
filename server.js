d57d4c8a35232b1631b8d24e4198001af3333655
Juan Dominguez <juandguezsuarez@gmail.com>
2019-07-02 22:15:45 +0200

    Server static file for production

--- server.js
+++ server.js
@@ -35,14 +35,7 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 //Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const port = process.env.PORT || 5000;
 
