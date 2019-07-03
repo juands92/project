b116966a8867e44311167d6c3bfd142a836b175b
Juan Domínguez <45494442+juands92@users.noreply.github.com>
2019-07-03 11:23:45 +0200

    Update ProfileGitHub.js

--- client/src/components/profile/ProfileGitHub.js
+++ client/src/components/profile/ProfileGitHub.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import isEmpty from '../../validation/is_empty';
 
 class ProfileGitHub extends Component {
   constructor() {
@@ -14,31 +15,34 @@ class ProfileGitHub extends Component {
     )
       .then(res => res.json())
       .then(data => {
-        this.setState({ repos: data });
+        if (Array.isArray(data)) this.setState({ repos: data });
       })
       .catch(err => console.log(err));
   }
 
   render() {
     const { repos } = this.state;
-
-    const listOfRepos = repos.map(repo => (
-      <div className="col-sm-6 mb-2" key={repo.id}>
-        <div className="card" style={{ height: "150px" }}>
-          <div className="card-body">
-            <h5 className="card-title text-info">{repo.name}</h5>
-            <p className="card-text">{repo.description}</p>
-            <span className="badge badge-info mr-1">
-              Stars: {repo.stargazers_count}
-            </span>
-            <span className="badge badge-danger mr-1">
-              Issues: {repo.open_issues_count}
-            </span>
+    let listOfRepos = null;
+    if(!isEmpty(repos)) {
+      listOfRepos = repos.map(repo => (
+        <div className="col-sm-6 mb-2" key={repo.id}>
+          <div className="card" style={{ height: "150px" }}>
+            <div className="card-body">
+              <h5 className="card-title text-info">{repo.name}</h5>
+              <p className="card-text">{repo.description}</p>
+              <span className="badge badge-info mr-1">
+                Stars: {repo.stargazers_count}
+              </span>
+              <span className="badge badge-danger mr-1">
+                Issues: {repo.open_issues_count}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    ));
+      ));
+    }
 
+   
     return (
       <div>
         <hr />
